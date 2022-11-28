42fb7166325e7a816cae8239a7ff59f69ac70fe7
Sourav Barui <74421679+im-sourav@users.noreply.github.com>
2022-11-28 18:37:17 +0530

    domne

--- piecs.js
+++ piecs.js
@@ -16,31 +16,22 @@ class Piec {
       if (this.collapsed) {
          drawImage(this.possibles[0].img, this.x, this.y, this.width, this.height);
       }
-
-
-
-
-      // if (this.collapsed) {
-      //    // drawImage(this.possibles[0].img, this.x, this.y, this.width, this.height);
-      // }
-      // else {
-      //    lineWidth(0.5);
-      //    stroke("#00ffff"); 
-      //    rect(this.x, this.y, this.width, this.height);
-      // }
    }
 
    showPossibles() {
-      if (this.out && !this.collapsed) {
+      if (!this.collapsed) {
+         
          let textSize = this.width * this.height / 100;
          textSize = textSize > 50 ? 50 : textSize;
          if (textSize > 5) {
-            fillStyle("#000000");
+            fillStyle("#ffffff");
             font(`${textSize}px sans-serif`)
             text(this.possibles.length, this.x + this.width / 2, this.y + this.height / 2, this.width, this.height)
          }
-
-         
+         if (this.out) {
+            fillStyle("#ff000011");
+            fillRect(this.x, this.y, this.width, this.height);
+         }
       }
    }
 
