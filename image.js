ddbca08e276dfc61130ac26ce015c63a52871272
Sourav Barui <74421679+im-sourav@users.noreply.github.com>
2022-12-02 20:42:52 +0530

    update images

--- image.js
+++ image.js
@@ -3,7 +3,7 @@ const _ctx_ = _cvs_.getContext('2d');
 
 const colors = ["#000000", "#ffffff", "#ff0000", "#00ff00",
     "#0000ff", "#ffff00", "#30ff3e", "#ff00ff",
-    "#a39268", "#1a62ff", "#ffd700", "#00980a",
+    "#a39268", "#1a62ff", "#ffd700", "#007007",
     "#bab8b8", "#202020", "#c0c0c0", "#808080"];
 
 const patternToImage = (pattern) => {
